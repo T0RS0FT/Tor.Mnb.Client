34e6cc73af42adee504b45c4b3a0c4c234f09e20
T0RS0FT <balazs.toronyi@gmail.com>
2025-02-22 10:27:18 +0100

    Add ServiceCollectionExtension

---
+++ src/Tor.MNB.Client/DependencyInjection/MnbServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Tor.MNB.Client.DependencyInjection
+{
+    public static class MnbServiceCollectionExtensions
+    {
+        public static void AddMnb(this IServiceCollection services)
+        {
+            services.AddScoped<IMnbClient, MnbClient>();
+        }
+    }
+}
