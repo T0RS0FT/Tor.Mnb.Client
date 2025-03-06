14e9cd490165eead62b5764c2abbf81e608083cc
T0RS0FT <balazs.toronyi@gmail.com>
2025-03-06 15:46:50 +0100

    Refactor

--- src/Tor.MNB.Client/MnbClient.cs
+++ src/Tor.MNB.Client/MnbClient.cs
@@ -8,12 +8,12 @@ namespace Tor.MNB.Client
 {
     public class MnbClient : IMnbClient
     {
+        private readonly MNBArfolyamServiceSoapClient client = new();
+
         public async Task<bool> HealthCheckAsync() => (await this.GetCurrenciesAsync())?.Count > 0;
 
         public async Task<List<string>> GetCurrenciesAsync()
         {
-            var client = new MNBArfolyamServiceSoapClient();
-
             var response = await client.GetCurrenciesAsync(new GetCurrenciesRequestBody());
 
             var result = XmlHelper.DeserializeXml<GetCurrenciesResponseModel>(response.GetCurrenciesResponse1.GetCurrenciesResult);
@@ -23,8 +23,6 @@ namespace Tor.MNB.Client
 
         public async Task<GetInfoResult> GetInfoAsync()
         {
-            var client = new MNBArfolyamServiceSoapClient();
-
             var response = await client.GetInfoAsync(new GetInfoRequestBody());
 
             var result = XmlHelper.DeserializeXml<GetInfoResponseModel>(response.GetInfoResponse1.GetInfoResult);
